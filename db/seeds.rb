120453f8db5e1876ec8a99bd6113ddcb1a224873
jbag123 <joshcone@googlemail.com>
2018-01-21 19:08:09 +0000

    commiting assignment 16

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,28 @@ require 'random_data'
 # #1
   Post.create!(
 # #2
-    title:  RandomData.random_sentence,
-    body:   RandomData.random_paragraph
+        title:  RandomData.random_sentence,
+        body:   RandomData.random_paragraph
   )
 end
+# Create unique post and check if exists
+
+Post.find_or_create_by!(title: "Unique Title", body: "Unique Body")
+
 posts = Post.all
+
 
 # Create Comments
 # #3
 100.times do
   Comment.create!(
 # #4
-    post: posts.sample,
-    body: RandomData.random_paragraph
+        post: posts.sample,
+        body: RandomData.random_paragraph
   )
 end
+
+Comment.find_or_create_by!(body: "Unique Comment Title", post: Post.find(51))
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
