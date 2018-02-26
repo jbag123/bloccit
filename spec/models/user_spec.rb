123c532659e4f3c6604e89054d7c9a228cd6ed5b
jbag123 <joshcone@googlemail.com>
2018-02-26 12:37:22 +0000

    commiting checkpoint 27

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,18 +5,16 @@ RSpec.describe User, type: :model do
   let(:user) { User.create!(name: 'Bloccit User', email: "user@bloccit.com", password: "password") }
 
   it { is_expected.to have_many(:posts) }
+  it { is_expected.to have_many(:comments) }
 
-  # valdate name with shoulda
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
 
-  # valdate email with shoulda
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to validate_length_of(:email).is_at_least(3) }
   it { is_expected.to allow_value("user@bloccit.com").for(:email) }
 
-  # valdate password with shoulda
   it { is_expected.to validate_presence_of(:password) }
   it { is_expected.to have_secure_password }
   it { is_expected.to validate_length_of(:password).is_at_least(6) }
@@ -59,7 +57,7 @@ RSpec.describe User, type: :model do
         expect(user.admin?).to be_falsey
       end
     end
-    
+
     context "admin user" do
       before do
         user.admin!
