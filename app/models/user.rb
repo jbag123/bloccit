b5a4cfc459b8315a6842f094b99a5de5f74913e1
jbag123 <joshcone@googlemail.com>
2018-02-27 21:29:13 +0000

    commiting checkpoint 28

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
+
   before_save { self.email = email.downcase if email.present? }
   before_save {
                 self.name = name.split.map {|word| word.capitalize }.join(" ") if name.present?
