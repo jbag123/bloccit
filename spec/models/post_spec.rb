28bf26c450afd410eee893c676b23997371a7764
jbag123 <joshcone@googlemail.com>
2018-02-28 21:30:29 +0000

    commiting assignment 28

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -74,5 +74,17 @@ require 'rails_helper'
           expect(post.rank).to eq (old_rank - 1)
         end
       end
+
+      describe "#create_vote" do
+        it "calls #create_vote when a post is created" do
+          post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_sentence, user: user)
+          expect(post).to receive(:create_vote)
+          post.save
+        end
+
+        it "associates the vote with the owner of the post" do
+          expect(post.votes.first.user).to eq(post.user)
+        end
+      end
     end
  end
