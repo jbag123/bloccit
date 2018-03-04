f24f55dfde8e0fbb3cd09682d700c96abdc81ac1
jbag123 <joshcone@googlemail.com>
2018-03-04 18:37:01 +0000

    partial commit of checkpoint 29

---
+++ app/mailers/favorite_mailer.rb
@@ -0,0 +1,15 @@
+class FavoriteMailer < ApplicationMailer
+  default from: "joshcone@googlemail.com"
+
+  def new_comment(user, post, comment)
+
+    headers["Message-ID"] = "<comments/#{comment.id}@powerful-everglades-69910>"
+    headers["In-Reply-To"] = "<post/#{post.id}@powerful-everglades-69910>"
+    headers["References"] = "<post/#{post.id}@powerful-everglades-69910>"
+    @user = user
+    @post = post
+    @comment = comment
+
+    mail(to: @user.email, subject: "New comment on #{post.title}")
+end
+end
